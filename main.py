5df3bc142b6b4551acd71b7461af4ccb888144a5
andhika pratama putra <120385412+andhikaprttp@users.noreply.github.com>
2023-05-21 10:55:21 +0700

    update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 # Sandi sungai merupakan sandi sederhana yang memiliki 8 kolom dan 5 baris, sehingga sering juga disebut sandi 5 per 8
 # Cara penggunaannya adalah tulislah pesan dengan huruf yang berseberangan atau berlawanan. Misalnya pesan ADA BAHAYA JANGAN LEWAT, maka ditulis dengan sandi OZO LOWOEO VOQPOQ BYHOS.
+# Sumber :https://www.detik.com/bali/berita/d-6387525/15-jenis-sandi-pramuka-dan-contoh-gerakannya
 
 
 
