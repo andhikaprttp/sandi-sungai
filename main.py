dd82399276693e036cc250eb7fb0b389579ffc04
andhika pratama putra <120385412+andhikaprttp@users.noreply.github.com>
2023-05-21 10:54:47 +0700

    update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 # Program Sandi Sungai 
 
+# Sandi sungai merupakan sandi sederhana yang memiliki 8 kolom dan 5 baris, sehingga sering juga disebut sandi 5 per 8
+# Cara penggunaannya adalah tulislah pesan dengan huruf yang berseberangan atau berlawanan. Misalnya pesan ADA BAHAYA JANGAN LEWAT, maka ditulis dengan sandi OZO LOWOEO VOQPOQ BYHOS.
+
+
 
 
 # Membuat kamus sandi Sungai Pramuka
